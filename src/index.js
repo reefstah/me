5f5ed5f04089bcaa09480633a429dd1c8b308916
reef <public@ralph.tech>
2018-07-14 01:30:42 +0200

    static build

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import Vue from 'vue';
 import fetch from 'isomorphic-fetch';
 
-
-var message = 'hello';
+const message = 'hello';
 
 // Step 1: Create a Vue instance
-
 const app = new Vue({
-    render (h) {
-        return <div>{message}</div>
-    }
+  render(h) {
+    return <div id="app">{message}</div>;
+  },
 });
 
 // Step 2: Create a renderer
-const renderer = require('vue-server-renderer').createRenderer();
-
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('src/index.template.html', 'utf-8'),
+});
 
 // Step 3: Render the Vue instance to HTML
-renderer.renderToString(app).then(html => {
-    console.log(html)
-}).catch(err => {
-    console.error(err)
-});
+renderer
+  .renderToString(app)
+  .then(html => {
+    console.log(html);
+  })
+  .catch(err => {
+    console.error(err);
+  });
